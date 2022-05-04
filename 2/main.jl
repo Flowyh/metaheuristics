641f59534ced5e9d3677035021c250c0ba90f7c7
Flowyh <altermydestiny@gmail.com>
2022-05-04 23:59:27 +0200

    Working random graphs tabuSearch testing

--- 2/main.jl
+++ 2/main.jl
@@ -1,21 +1,27 @@
-include("./tabuSearch.jl")
-using .TabuSearch
-using TSPLIB
+include("./testing.jl")
+include("../1/generate.jl")
 
 export timeCriterion, iterationsCriterion
 
 function usage()
    println("""
-   Usage: julia zad2.jl
+   Usage: julia main.jl
    ARGS:
-   1: path to TSP file
+   1: path to TSP file or data type: ["hardcoded", "random"]
    2: starting solution [krand, 2opt, nb, nnb]
    3: move type [swap, insert, invert]
-   4: stop criteria [time, iteration]
-   5: tabu size [uint]
-   6: aspiration in percent [0-100]
-   7: backtrack size [uint]
-   8: stagnation limit [uint]
+   4: stop criterion [time, iteration]
+   5: stop criterion limit [seconds, number of iterations]
+   6: tabu size [uint]
+   7: aspiration as percentage [0-100]
+   8: backtrack size [uint]
+   9: stagnation limit [uint]
+   10: other funcs (comma separated) from: [krand, 2opt, nb, nnb] or "all"
+   11: repeats (if mode == hardcoded or random) [uint]
+   12: start number of nodes (if mode == random) [uint]
+   13: step number of nodes (if mode == random) [uint]
+   14: end number of nodes (if mode == random) [uint]
+   15: random type [euclidean, assymetric]
    """)
 end
 
@@ -25,8 +31,8 @@ function main(args::Array{String})
   moves = Dict("swap" => moveSwap, "insert" => moveInsert, "invert" => moveInvert)
   stopCriteria = Dict("time" => timeCriterion, "iteration" => iterationsCriterion)
 
-  if (length(ARGS) < 8)
-    println("Please provide 8 arguments.")
+  if (length(ARGS) < 9)
+    println("Please provide at least 9 arguments.")
     usage()
     exit(1)
   end
@@ -35,8 +41,7 @@ function main(args::Array{String})
     exit(1)    
   end
   try
-    
-    tsp = readTSP(args[1])
+    mode = args[1]
     #tsp = openTSPFile()
     startingFunc = startingAlgs[args[2]]
     move = moves[args[3]]
@@ -46,25 +51,88 @@ function main(args::Array{String})
     aspiration = parse(Int, args[7])
     backtrackSize = parse(Int, args[8])
     stagnationLimit = parse(Int, args[9])
+    other_funcs = Array{Function}([tabuSearch, values(startingAlgs)...])
+    if (length(args) >= 10 && args[10] != "all")
+      other_funcs = Array{Function}([tabuSearch])
+      funcs = split(args[10], ",")
+      for func in funcs
+        if (haskey(startingAlgs, func))
+          push!(tabuSearch, startingAlgs[func])
+        end
+      end
+    end
 
-    dict_tsp = structToDict(tsp)
-    initial_path = startingFunc(dict_tsp)
-  
-    println("Tabu search distance: $(tabuSearch(
-      initial_path,
-      dict_tsp[:dimension],
-      dict_tsp[:weights],
-      move,
-      stopCriterion(stopCritAmount), # timeCriterion(10),
-      tabuSize,        #fld(dict_tsp[:dimension], 2),# 7,# trunc(Int, 2 * dict_tsp[:dimension] /  (1 + sqrt(5))),
-      0.01 * aspiration,
-      backtrackSize,
-      stagnationLimit
-      )[2]
-    )")
-    println("Two opt distance: $(nodeWeightSum(initial_path, dict_tsp[:weights]))")
-
+    if (mode == "hardcoded")
+      if (length(args) < 11)
+        println("Please provide at least 11 arguments.")
+        usage()
+        exit(1)
+      end
+      tabuTSPTest(
+        other_funcs,
+        parse(Int, args[11]),
+        [
+          move,
+          stopCriterion,
+          stopCritAmount,
+          tabuSize,        #fld(dict_tsp[:dimension], 2),# 7,# trunc(Int, 2 * dict_tsp[:dimension] /  (1 + sqrt(5))),
+          0.01 * aspiration,
+          backtrackSize,
+          stagnationLimit
+        ]
+      )
+    elseif (mode == "random")
+      if (length(args) < 15)
+        println("Please provide at least 15 arguments.")
+        usage()
+        exit(1)
+      end
+      if (args[15] == "euclidean")
+        random = generateEuclidean
+      elseif (args[15] == "assymetric")
+        random = generateAsymmetric
+      else
+        println("Invalid random mode provided.")
+        usage()
+        exit(1)
+      end
+      tabuRandomTest(
+        other_funcs,
+        parse(Int, args[11]),
+        parse(Int, args[12]),
+        parse(Int, args[13]),
+        parse(Int, args[14]),
+        random,
+        [
+          move,
+          stopCriterion,
+          stopCritAmount,
+          tabuSize,        #fld(dict_tsp[:dimension], 2),# 7,# trunc(Int, 2 * dict_tsp[:dimension] /  (1 + sqrt(5))),
+          0.01 * aspiration,
+          backtrackSize,
+          stagnationLimit
+        ]
+      )
+    else
+      tsp = readTSP(args[1])
+      dict_tsp = structToDict(tsp)
+      initial_path = startingFunc(dict_tsp)
+      println("Tabu search distance: $(tabuSearch(
+        initial_path,
+        dict_tsp[:dimension],
+        dict_tsp[:weights],
+        move,
+        stopCriterion(stopCritAmount), # timeCriterion(10),
+        tabuSize,        #fld(dict_tsp[:dimension], 2),# 7,# trunc(Int, 2 * dict_tsp[:dimension] /  (1 + sqrt(5))),
+        0.01 * aspiration,
+        backtrackSize,
+        stagnationLimit
+        )[2]
+      )")
+      println("Two opt distance: $(nodeWeightSum(initial_path, dict_tsp[:weights]))")
+    end
   catch e
+    throw(e)
     println(e)
     exit(1)
   end
--- 2/testing.jl
+++ 2/testing.jl
@@ -7,29 +7,29 @@ import JSON
 function hardcodedData()
   return Dict(
     "bayg29.tsp" => 1610,
-    # "a280.tsp" => 2579,
-    # "ch130.tsp" => 6100,
-    # "brg180.tsp" => 1950,
-    # "lin105.tsp" => 14379,
-    # "pr76.tsp" => 108159,
-    # "ulysses16.tsp" => 6859,
-    # "tsp225.tsp" => 3916,
-    # "rd100.tsp" => 7910,
-    # "gr202.tsp" => 40160,
-    # "eil76.tsp" => 538,
-    # "ch150.tsp" => 6528,
-    # "berlin52.tsp" => 7542,
-    # "att48.tsp" => 10628,
-    # "eil51.tsp" => 426,
-    # "fri26.tsp" => 937,
-    # "gr96.tsp" => 55209,
-    # "st70.tsp" => 675,
-    # "burma14.tsp" => 3323,
-    # "hk48.tsp" => 11461,
-    # "bays29.tsp" => 2020,
-    # "brazil58.tsp" => 25395,
-    # "dantzig42.tsp" => 699,
-    # "rat99.tsp" => 1211
+    "a280.tsp" => 2579,
+    "ch130.tsp" => 6100,
+    "brg180.tsp" => 1950,
+    "lin105.tsp" => 14379,
+    "pr76.tsp" => 108159,
+    "ulysses16.tsp" => 6859,
+    "tsp225.tsp" => 3916,
+    "rd100.tsp" => 7910,
+    "gr202.tsp" => 40160,
+    "eil76.tsp" => 538,
+    "ch150.tsp" => 6528,
+    "berlin52.tsp" => 7542,
+    "att48.tsp" => 10628,
+    "eil51.tsp" => 426,
+    "fri26.tsp" => 937,
+    "gr96.tsp" => 55209,
+    "st70.tsp" => 675,
+    "burma14.tsp" => 3323,
+    "hk48.tsp" => 11461,
+    "bays29.tsp" => 2020,
+    "brazil58.tsp" => 25395,
+    "dantzig42.tsp" => 699,
+    "rat99.tsp" => 1211
   )
 end
 
@@ -109,7 +109,82 @@ function tabuTSPTest(functions::Array{Function}, k::Int, tabu_params::Array{Any}
   for func in functions
     file_str = "./jsons/$(func)-k$k-$now.json"
     if (func == tabuSearch)
-      file_str = "./jsons/$(func)-mv$(String(nameof(mv))[5:end])-s$limit-ts$ts-at$at-bs$bs-sl$sl-t$(Threads.nthreads())-$now.json"
+      file_str = "./jsons/$(func)-k$k-mv$(String(nameof(mv))[5:end])-st$limit-ts$ts-at$at-bs$bs-sl$sl-t$(Threads.nthreads())-$now.json"
+    end
+    open(file_str, "w") do io
+      JSON.print(io, results[func])
+    end;
+  end
+end
+
+function tabuRandomTest(
+  functions::Array{Function}, 
+  k::Int, start::Int, 
+  step::Int, 
+  s_end::Int, 
+  random::Function, 
+  tabu_params::Array{Any}
+)
+  results = Dict()
+
+  for func in functions
+    results[func] = Dict()
+  end
+
+  (mv, stopFun, limit, ts, at, bs, sl) = tabu_params
+  stop = stopFun(limit)
+  
+  for key in keys(results)
+    results[key] = Dict("time" => Dict(), "prd" => Dict(), "best" => Dict())
+  end
+  reg = r"([0-9]+)"
+  for n in start:step:s_end
+    println("PROBLEM: $n")
+    (nodes, distances) = random(n, 5n)
+    tsp_data = Dict(:dimension => n, :weights => distances)
+    best_func = typemax(Int)
+    best_dist_funcs = Dict()
+    for func in functions
+      results[func]["time"][n] = []
+      results[func]["best"][n] = []
+      results[func]["prd"][n] = []
+      if (func == tabuSearch)
+        path = twoopt(tsp_data)
+        (_, best_dist, time) = testTabuSearch(path, tsp_data[:dimension], tsp_data[:weights], mv, stop, ts, at, bs, sl)
+        push!(results[func]["time"][n], time)
+      else
+        best_path = krandom(tsp_data, 1)
+        best_dist = nodeWeightSum(best_path, tsp_data[:weights])
+        for i in 1:k
+          test_start = time_ns()
+          if (func == twoopt) path = func(tsp_data, best_path)
+          elseif (func == krandom) path = func(tsp_data, k)
+          else path = func(tsp_data, 1) end
+          test_end = time_ns()
+          time = (test_end - test_start) * 1e-6
+          dist = nodeWeightSum(path, tsp_data[:weights])
+          if (dist < best_dist)
+            best_dist = dist
+            best_path = path
+          end
+          push!(results[func]["time"][n], time)
+        end
+      end
+      best_dist_funcs[func] = best_dist
+      best_func = (best_func > best_dist ? best_dist : best_func)
+      push!(results[func]["best"][n], best_dist)
+    end
+    for func in functions
+      push!(results[func]["prd"][n], prd(best_dist_funcs[func], best_func))
+    end
+  end
+
+  now = Dates.now()
+  isdir("./jsons") || mkdir("./jsons")
+  for func in functions
+    file_str = "./jsons/$(func)-r$(String(nameof(random))[9:end])-k$k-b$start-s$step-e$s_end-$now.json"
+    if (func == tabuSearch)
+      file_str = "./jsons/$(func)-r$(String(nameof(random))[9:end])-k$k-b$start-s$step-e$s_end-mv$(String(nameof(mv))[5:end])-st$limit-ts$ts-at$at-bs$bs-sl$sl-t$(Threads.nthreads())-$now.json"
     end
     open(file_str, "w") do io
       JSON.print(io, results[func])
